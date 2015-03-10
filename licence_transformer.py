47cd00ef635e106c20280a75cbdd2e3962e8844f
Lorenz Fischer <lorenz.fischer@gmail.com>
2015-03-10 11:07:41 +0100

    pushed version|

---
+++ licence_transformer.py
@@ -0,0 +1,24 @@
+import sys
+import json
+import datetime
+
+while True:
+  line = sys.stdin.readline()
+  if not line:
+    break
+  raw_record = json.loads(line)
+
+  licence_record = {
+      "source_url": raw_record['source_url'],                             # Required
+      "company_name": raw_record['Name'],                                 # Required
+      "company_jurisdiction": "Switzerland",                              # Required
+      "licence_jurisdiction": "Switzerland",                              # Required. Where the activity is permitted
+      "regulator": "Swiss Financial Market Supervisory Authority FINMA",  # Optional
+      "category": "Financial",                                            # Optional. Must be 'Financial' or 'Business'(at the moment)
+      #"licence_number": "149923-AB-33",                                  # Optional
+      "jurisdiction_classification": raw_record['Licensing'],             # Optional. Can also be an array of strings.
+      "status": "Discontinuing",                                          # Optional
+      "confidence": "HIGH",                                               # Optional. Must be HIGH, MEDIUM, or LOW
+      "sample_date": raw_record['sample_date']                            # Required
+  }
+  print json.dumps(licence_record, ensure_ascii=False).encode('utf-8')
---
+++ scraper.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+import json
+import datetime
+import turbotlib
+import urllib2
+import zipfile
+import requests
+import xlrd
+import pprint
+
+import io
+
+# this code is copied form here: https://github.com/metaodi/finma-scraper/blob/master/mission_838/scraper.py
+
+def get_rows(content):
+    workbook = xlrd.open_workbook(file_contents=content)
+    worksheet = workbook.sheet_by_index(0)
+    # Extract the row headers
+    header_row = worksheet.row_values(3)
+    # print header_row
+    rows = []
+    for row_num in range(worksheet.nrows):
+        # Data columns begin at row count 4 (5 in Excel)
+        if row_num >= 4:
+            rows.append(dict(zip(
+                header_row,
+                worksheet.row_values(row_num)
+            )))
+    return rows
+
+FINMA_URL = 'https://www.finma.ch/institute/xls_e/ebeh_status2.xlsx'
+#FINMA_URL = 'file:///Users/lfischer/Desktop/ebeh_status2.xlsx'
+r = requests.get(FINMA_URL)
+
+for row in get_rows(r.content):
+    # headers are:
+    # Name	City	Licensing	Bank type			Securities dealer type
+    name = row['Name']
+    if name and len(name) and not name.startswith(u'Total banks and securities dealers discontinuing their business activities'):
+        del row[None]
+        row['sample_date'] = datetime.datetime.now().isoformat()
+        row['source_url'] = FINMA_URL # so we can copy it in the transformer
+        print(json.dumps(row, ensure_ascii=False).encode('utf-8'))
